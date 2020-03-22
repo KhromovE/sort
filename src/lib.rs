fc6ab2179d4d0da95052650ee8a68d8db11dc656
Egor <khromove@gmail.com>
2020-03-22 17:09:20 +0700

    the better way

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@ mod shaker_sort;
 mod insertion_sort;
 mod quick_sort;
 
-pub use bubble_sort::bubble_sort as bubble_sort;
-pub use shaker_sort::shaker_sort as shaker_sort;
-pub use insertion_sort::insertion_sort as insertion_sort;
-pub use quick_sort::quick_sort as quick_sort;
+pub use bubble_sort::*;
+pub use shaker_sort::*;
+pub use insertion_sort::*;
+pub use quick_sort::*;
 
